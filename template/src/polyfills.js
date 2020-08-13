8a76199d2602ace2648f98be6d4db0d98b8def8e
Paul <p.simkevi@gmail.com>
2020-08-13 20:24:46 +0200

    add `nomodule` `polyfills.js` support

---
+++ template/src/polyfills.js
@@ -0,0 +1,9 @@
+/**
+ * Should only be used for importing polyfills.
+ *
+ * This file will bundled separately from the main application
+ * and the bundle will only execute in `nomodule` browsers such as IE11.
+ *
+ * NOTE: polyfills for modern browsers should be included in `index.ts` instead
+ * NOTE 2: language feature polyfills should be added via `babel` and/or `typescript`
+ */
