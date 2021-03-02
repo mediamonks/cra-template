658be5842357f6d9c3fb31a4768a9a9b764af2c9
Paul <p.simkevi@gmail.com>
2021-03-02 20:57:44 +0100

    update "standard" to use `@mediamonks/eslint-config-react`

--- standard/template/src/hooks/useMount.ts
+++ standard/template/src/hooks/useMount.ts
@@ -1,10 +1,9 @@
 import { EffectCallback, useEffect } from 'react';
 
 /**
- * @param fn - function, which is triggered on mount. If the function returns a callback, it will be called on unmount.
+ * @param callback - function, which is triggered on mount. If the function returns a callback, it will be called on unmount.
  */
-const useMount = (fn: EffectCallback): void => {
-  useEffect(fn, []);
-};
-
-export default useMount;
+export default function useMount(callback: EffectCallback): void {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(callback, []);
+}
